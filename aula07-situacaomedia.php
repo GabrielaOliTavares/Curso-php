ed0c99d37aae1f9f2111e6493803396ba097f1d0
GabrielaOliTavares <145030166+GabrielaOliTavares@users.noreply.github.com>
2023-11-15 20:55:51 -0300

    aula07-situacaomedia.php

--- aula07-situacaomedia.php
+++ aula07-situacaomedia.php
@@ -10,7 +10,7 @@
     <?php
         $nota1 = $_GET["n1"];
         $nota2 = $_GET["n2"];
-        $m = ($nota1+$nota2)/2;
+        $m = ($nota1+$nota2)/2; #nao pode tirar o parentese se não ira dividir primeiro
         echo "A media entre $nota1 e $nota2 e $m <br/>";
         echo "A situacao do aluno e " . (($m<6)?"REPROVADO":"APROVADO");
     ?>
